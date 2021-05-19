b3095c3514e0314c6c1e99de1bb0e422a00564ca
enaguero <enaguero@gmail.com>
2021-05-19 19:00:29 +0000

    Code refactor to use a function to generate the random index

--- src/app.js
+++ src/app.js
@@ -5,38 +5,31 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+let who = ["the dog", "my granma", "his turtle", "my bird"];
+let what = ["eat", "pissed", "crushed", "broked"];
+let when = [
+  "before the class",
+  "right in time",
+  "when I finished",
+  "during my lunch",
+  "while I was praying"
+];
+
+let where = ["my home"];
+
+function generateRandomIndex(arr) {
+  return Math.floor(Math.random() * arr.length);
+}
+
 window.onload = function() {
   //write your code here
 
-  let who = ["the dog", "my granma", "his turtle", "my bird"];
-  let what = ["eat", "pissed", "crushed", "broked"];
-  let when = [
-    "before the class",
-    "right in time",
-    "when I finished",
-    "during my lunch",
-    "while I was praying"
-  ];
+  let excuse = `${who[generateRandomIndex(who)]} ${
+    what[generateRandomIndex(what)]
+  } 
+  ${when[generateRandomIndex(when)]}
 
-  let whoNumberRandom = 0;
-  let whatNumberRandom = 0;
-  let whenNumberRandom = 0;
-
-  for (let i = 0; i < who.length; i++) {
-    whoNumberRandom = Math.floor(Math.random() * i);
-  }
-
-  for (let j = 0; j < what.length; j++) {
-    whatNumberRandom = Math.floor(Math.random() * j);
-  }
-
-  for (let k = 0; k < when.length; k++) {
-    whenNumberRandom = Math.floor(Math.random() * k);
-  }
-
-  document.getElementById("excuse").innerHTML = `${who[
-    whoNumberRandom
-  ].toLocaleUpperCase()} ${what[whatNumberRandom].toUpperCase()} ${when[
-    whenNumberRandom
-  ].toUpperCase()}`;
+  ${where[generateRandomIndex(where)]}
+  `;
+  document.getElementById("excuse").innerHTML = excuse.toUpperCase();
 };
